c5ebafc6a6056caf84eb28711fff9c419b489cec
Lucine Madadi <lucine.madadi@ibm.com>
2018-10-24 10:30:12 +0200

    This commit includes:
    - Moving test cases to `test_assets` dir
    - Modifying the test cases classes to include the right path to
     test cases.
    - Removing TODO comments.

--- pkg/havener/exec_test.go
+++ pkg/havener/exec_test.go
@@ -1,22 +1,16 @@
 package havener
 
 import (
-	"fmt"
-	// "io/ioutil"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	// "encoding/json"
+	"os"
 	"gopkg.in/yaml.v2"
 )
 
 var _ = Describe("Exec", func() {
-
-	// TODO Add test case to check a string that does not have a shell operator.
-	// TODO Add test case with more than one shell operator.
-	// TODO Add test case with command and pipe into another command, e.g. `ls -l | wc -l`.
-
+	pwDir, _ := os.Getwd()
 	It("should replace correct inline shell statements with commands", func() {
-		input, err := ProcessConfigFile("correct_commands_test.yml")
+		input, err := ProcessConfigFile(pwDir + "/../../test_assets/correct_commands_test.yml")
 		if err != nil {
 			panic(err)
 		}
@@ -43,14 +37,12 @@ releases:
       UAA_ADMIN_CLIENT_SECRET: havener
 `
 		Expect(string(input2)).To(BeEquivalentTo(expected))
-		fmt.Println(input2)
-		fmt.Println(expected)
 
 	})
 
 
 	It("should replace correct inline shell statements with commands; with no override section", func() {
-		input, err := ProcessConfigFile("no_overrides_test.yml")
+		input, err := ProcessConfigFile(pwDir + "/../../test_assets/no_overrides_test.yml")
 		if err != nil {
 			panic(err)
 		}
@@ -65,28 +57,27 @@ releases:
   overrides: null
 `
 		Expect(string(input2)).To(BeEquivalentTo(expected))
-		fmt.Println(input2)
-		fmt.Println(expected)
 
 	})
 
 
 
 	It("should return an error when there's a false inline shell statement", func() {
-		input := "incorrect_commands_test.yml"
+		input := pwDir + "/../../test_assets/incorrect_commands_test.yml"
 		_, err := ProcessConfigFile(input)
 
 		expected := `failed to run command: abcd
 error message: exit status 127`
 
 		Expect(err.Error()).To(BeEquivalentTo(expected))
-		fmt.Println(err.Error())
 
 	})
 
 
+
+
 	It("should leave the program unchanged in case there's no inline shell statements", func() {
-		input, err := ProcessConfigFile("no_commands_test.yml")
+		input, err := ProcessConfigFile(pwDir + "/../../test_assets/no_commands_test.yml")
 		if err != nil {
 			panic(err)
 		}
@@ -113,8 +104,6 @@ releases:
       UAA_ADMIN_CLIENT_SECRET: havener
 `
 		Expect(string(input2)).To(BeEquivalentTo(expected))
-		fmt.Println(input2)
-		fmt.Println(expected)
 
 	})
 
