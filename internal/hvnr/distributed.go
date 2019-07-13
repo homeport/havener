d5b6e3b6bc44647ddc5dad0bcaf9aaf61f4255b0
Matthias Diester <mddiester@gmail.com>
2019-07-13 10:14:08 +0200

    Update distributed.go

--- internal/hvnr/distributed.go
+++ internal/hvnr/distributed.go
@@ -1,4 +1,4 @@
-// Copyright © 2018 The Havener
+// Copyright © 2019 The Havener
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
